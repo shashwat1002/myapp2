3f2caeb453aa63e4ea91d5188f8eb0adc45fc53c
shashwat1002 <singhshashwat1002@gmail.com>
2018-01-03 15:52:31 +0530

    new changes

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
+
+def post_list(request):
+    return render(request, '/templates/blog/post_list.html')
+
+
 # Create your views here.
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.expanduser('~/my-app')
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE']='mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+
+
+application =StaticFilesHandler( get_wsgi_application())
