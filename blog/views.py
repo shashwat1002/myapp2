3e0da5857510f489b0422becdaa3c644b6bddc77
shashwat1002 <singhshashwat1002@gmail.com>
2018-01-03 16:03:33 +0530

    new commits

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def post_list(request):
-    return render(request, '/templates/blog/post_list.html')
+    return render(request, 'blog/post_list.html')
 
 
 # Create your views here.
